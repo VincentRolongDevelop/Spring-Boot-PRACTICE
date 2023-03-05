fef0d7277c0fea3dd08217c9f34a25cd07f7fe5c
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2023-03-04 22:01:47 -0500

    Web usage and html index added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /*
  *VIDEO 6
@@ -8,9 +11,12 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class DemoController {
-	
-
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
 	}
+}
 
 
 	
