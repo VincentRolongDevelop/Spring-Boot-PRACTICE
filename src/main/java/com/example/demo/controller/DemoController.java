65e7fe406ff3c9d999fe3df52830c87e67629f0a
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2023-03-04 20:10:04 -0500

    @Controller annotation added

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/*
+ *VIDEO 6
+ *This annotation is used when you have a class with a logic of a controller o a business
+ */
+@Controller
+public class DemoController {
+	
+
+	}
+
+
+	
+
