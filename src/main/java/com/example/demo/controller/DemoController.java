4d6b7164e5b0e03c04bc94cc3c491a73ec60c003
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2023-03-06 17:48:10 -0500

    Data base added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Person;
+import com.example.demo.repo.IPersonRepo;
 
 /*
  *VIDEO 6
@@ -12,8 +16,17 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class DemoController {
+	
+	@Autowired
+	private IPersonRepo repo;
+	
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		Person p= new Person();
+		p.setIdPerson(1);
+		p.setName("Vincent");
+		repo.save(p);
+		
 		model.addAttribute("name", name);
 		return "index";
 	}
